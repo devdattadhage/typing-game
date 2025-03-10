f292e28c46f6c2d64c053c1b16d8f848b257a185
amanKumarBharti <183721382+Aman-bhart@users.noreply.github.com>
2025-03-11 01:13:09 +0530

    breaks into functions

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// deno-lint-ignore-file
-
 const generatePara = (text) => {
   const para = document.querySelector("#typing-text");
   text.split("").forEach((char) => {
@@ -11,29 +9,50 @@ const generatePara = (text) => {
   return para;
 };
 
+const handleBackspace = (index, spanElement) => {
+  if (index <= 0) {
+    return index;
+  }
+
+  spanElement.classList.remove("validKey", "invalidKey");
+
+  return index - 1;
+};
+
+const handleShift = (index) => {
+  return index;
+};
+
+const handleNormalKey = (index, spanElement, key) => {
+  if (key === spanElement.innerText) {
+    spanElement.classList.add("validKey");
+    return index + 1;
+  }
+
+  spanElement.classList.add("invalidKey");
+  return index + 1;
+};
+
+const handleKeysValid = (key, index, spanElements) => {
+  const specialKey = {
+    Backspace: handleBackspace,
+    Shift: handleShift,
+  };
+
+  if (specialKey[key]) {
+    return specialKey[key](index, spanElements[index - 1]);
+  }
+
+  return handleNormalKey(index, spanElements[index], key);
+};
+
 const main = () => {
   const text = "Welcome To Typing Club";
   const para = generatePara(text);
 
   let index = 0;
   document.addEventListener("keydown", (event) => {
-    if (event.key === "Shift") return;
-
-    const spanElements = para.children;
-    if (event.key === "Backspace" && index > 0) {
-      index--;
-      spanElements[index].classList.remove("validKey", "invalidKey");
-    } else if (
-      index < spanElements.length &&
-      event.key === spanElements[index].innerText
-    ) {
-      spanElements[index].classList.add("validKey");
-      index++;
-    } else if (index < spanElements.length) {
-      spanElements[index].classList.add("invalidKey");
-      index++;
-    }
-
+    index = handleKeysValid(event.key, index, para.children, event);
     console.log(event.key);
   });
 };
